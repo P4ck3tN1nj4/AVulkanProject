5a17ee8cd3861a6a41cb6095726ca3007e0feed8
P4ck3tN1nj4 <124211622+P4ck3tN1nj4@users.noreply.github.com>
2025-05-09 21:13:11 -0700

    Window Intagration
    Initializing the window

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
-#include <iostream>
+#include <precomp.h>
+#include <GLFW/glfw3.h>
+#include <glfw_initialization.h>
+#include <glfw_monitor.h>
+#include <glfw_window.h>
 
+std::int32_t main(std::int32_t argc, gsl::zstring* argv) {
+  const veng::GlfwInitialization _glfw;
 
+  veng::Window window("Vulkan Engine", {800, 600});
+  window.TryMoveToMonitor(1);  // if you want to change monitor
 
-using namespace std;
+  while (!window.ShouldClose()) {
+    glfwPollEvents();  // not window specific
+  }
 
-int main()
-{
-	cout << "Hello CMake!" << endl;
-
-	return 0;
+  return EXIT_SUCCESS;
 }
